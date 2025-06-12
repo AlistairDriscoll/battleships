5e91a424f6a7e8462d87897a9ff04f04ef6050bb
Alistair Driscoll <pallythecoder@gmail.com>
2025-06-12 12:01:47 +0100

    Complete next video in tutorial, computer can now check shots

--- boats.py
+++ boats.py
@@ -44,20 +44,18 @@ def create_boats():
     ships = []
     boats = [5, 4, 3, 3, 2, 2]
     for b in boats:
-        print(f"Trying to find a position for the {b} length boat")
         boat = [-1]
         while boat[0] == -1:
             boat_start = randrange(99)
             boat_direction = randrange(1, 4)
             boat = check_boat(b, boat_start, boat_direction, taken)
         ships.append(boat)
-        print(ships)
         taken = taken + boat
 
     return ships, taken
 
 
-def show_board(taken):
+def show_board_c(taken):
     print("            BATTLESHIPS          ")
     print()
     print("     0  1  2  3  4  5  6  7  8  9")
@@ -68,12 +66,71 @@ def show_board(taken):
         for y in range(10):
             ch = " _ "
             if place in taken:
-                ch = " x "
+                ch = " o "
             row = row + ch
             place = place + 1
         print(x, " ", row)
 
 
-boats, taken = create_boats()
-print("All boats taken")
-show_board(taken)
+def get_shot_comp(guesses):
+
+    ok = "n"
+    while ok == "n":
+        try:
+            shot = randrange(99)
+            if shot not in guesses:
+                ok = "y"
+                guesses.append(shot)
+                break
+        except ValueError:
+            print("incorrect entry, please enter again")
+    return shot, guesses
+
+
+def show_board(hit, miss, comp):
+    print("            BATTLESHIPS          ")
+    print()
+    print("     0  1  2  3  4  5  6  7  8  9")
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in miss:
+                ch = " x "
+            elif place in hit:
+                ch = " o "
+            elif place in comp:
+                ch = " O "
+            row = row + ch
+            place = place + 1
+        print(x, " ", row)
+
+
+def check_shot(shot, ships, hit, miss, comp):
+    missed = 1
+    for i in range(len(ships)):
+        if shot in ships[i]:
+            ships[i].remove(shot)
+            missed = 0
+            if len(ships[i]) > 0:
+                hit.append(shot)
+            else:
+                comp.append(shot)
+    if missed == 1:
+        miss.append(shot)
+    print(hit)
+    return ships, hit, miss, comp
+
+
+hit = []
+miss = []
+comp = []
+guesses = []
+ships, taken = create_boats()
+show_board_c(taken)
+for i in range(50):
+    shot, guesses = get_shot_comp(guesses)
+    ships, hit, miss, comp = check_shot(shot, ships, hit, miss, comp)
+    show_board(hit, miss, comp)
